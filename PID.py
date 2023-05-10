992700cac53677c8d77c8e15e00bc705c662b223
Ghermoul Alaeddine <Ghermoul.ad@gmail.com>
2023-05-10 15:54:52 +0200

    Add files via upload

---
+++ PID.py
@@ -0,0 +1,81 @@
+from Model import RoboticArm
+import numpy as np
+
+class PID:
+    def __init__(self, kp=1.0, ki=0.0, kd=0.0, target=np.zeros(3), dt=0.01):
+        self.kp = kp  # proportional gain
+        self.ki = ki  # integral gain
+        self.kd = kd  # derivative gain
+        self.target = target  # target position
+        self.last_error = 0
+        self.integral = 0
+        self.arm = RoboticArm()  # create a robotic arm object
+        self.prev_error = np.zeros(3)  # previous error for derivative term
+        self.integral_error = np.zeros(3)  # integral error for integral term
+        self.pid = PIDController(kp, ki, kd, dt)  # create a PIDController object
+    def control(self, current_position, dt):
+    # calculate the error between the desired and current position
+        error = self.target - current_position
+
+    # calculate the derivative of the error
+        self.previous_error = error
+        derivative_error = (error - self.previous_error) / dt
+
+
+    # calculate the integral of the error
+        self.integral_error += error * dt
+
+    # calculate the control output
+        control_output = self.kp * error + self.kd * derivative_error + self.ki * self.integral_error
+
+    # return the control output
+        return control_output
+    def Calculate(self, current_value, target_value, dt):
+        error = target_value - current_value
+        self.integral_error += error * dt
+        derivative = (error - self.last_error) / dt
+        output = self.kp * error + self.ki * self.integral_error + self.kd * derivative
+        self.last_error = error
+        return output
+    # def control(self, current_position, dt=0.01):
+    #     # Compute the error between the current position and the target position
+    #     error = self.target - current_position
+
+    #     # Update the PID controller
+    #     self.pid.update(error = error, dt = dt)
+
+    #     # Compute the control output
+    #     control_output = self.pid.output()
+
+    #     # Compute the desired joint angles
+    #     desired_joint_angles = self.arm.inverse_kinematics(self.target)
+
+    #     # Apply the control output to the joint angles
+    #     new_joint_angles = desired_joint_angles + control_output
+
+    #     # Return the new joint angles
+    #     return new_joint_angles
+
+class PIDController:
+    def __init__(self, kp, ki, kd, dt=0.01):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.dt = dt
+        self.last_error = 0
+        self.integral_error = 0
+        #self.derivative_error = 0
+
+    def update(self, error, dt=0.01):
+        #self.error = 0.0
+        derivative_error = (error - self.last_error) / self.dt
+        self.integral_error += error * self.dt
+        self.last_error = error
+        return self.kp * self.last_error + self.ki * self.integral_error + self.kd * derivative_error
+
+    def reset(self):
+        self.last_error = 0
+        self.integral_error = 0
+
+    def output(self):
+        return self.update(error=0)
---
+++ PlotResponse.py
@@ -0,0 +1,73 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from Model import RoboticArm
+from PID import PID
+from PID import PIDController
+# Define the target position
+target_pos = np.array([1, 1, 1])
+
+# Initialize the robotic arm and the PID controller
+arm = RoboticArm()
+pid = PID()
+#pidcontroller = PIDController()
+
+# Set the simulation time and time step
+t_end = 10
+dt = 0.1
+t = np.arange(0, t_end, dt)
+
+# Initialize arrays for plotting data
+setpoint = np.zeros((len(t), 3))
+actual_pos = np.zeros((len(t), 3))
+error = np.zeros((len(t), 3))
+
+# Run the simulation
+for i in range(len(t)):
+    # Calculate the PID control output
+    control_output = pid.Calculate(arm.forward_kinematics(arm.joint_angles)[0], target_pos, dt)
+
+    # Update the joint angles using the control output
+    arm.update_joint_angles(control_output, dt)
+
+    # Store data for plotting
+    setpoint[i] = target_pos
+    actual_pos[i] = arm.forward_kinematics()
+    error[i] = pid.error
+
+# Plot Setpoint vs. actual position
+plt.figure()
+plt.plot(t, setpoint[:, 0], 'b--', label='Setpoint')
+plt.plot(t, actual_pos[:, 0], 'r-', label='Actual Position')
+plt.xlabel('Time (s)')
+plt.ylabel('Position (m)')
+plt.title('X-axis position')
+plt.legend()
+
+plt.figure()
+plt.plot(t, setpoint[:, 1], 'b--', label='Setpoint')
+plt.plot(t, actual_pos[:, 1], 'r-', label='Actual Position')
+plt.xlabel('Time (s)')
+plt.ylabel('Position (m)')
+plt.title('Y-axis position')
+plt.legend()
+
+plt.figure()
+plt.plot(t, setpoint[:, 2], 'b--', label='Setpoint')
+plt.plot(t, actual_pos[:, 2], 'r-', label='Actual Position')
+plt.xlabel('Time (s)')
+plt.ylabel('Position (m)')
+plt.title('Z-axis position')
+plt.legend()
+
+# Plot Error vs. time
+plt.figure()
+plt.plot(t, error[:, 0], 'r-', label='X-axis Error')
+plt.plot(t, error[:, 1], 'g-', label='Y-axis Error')
+plt.plot(t, error[:, 2], 'b-', label='Z-axis Error')
+plt.xlabel('Time (s)')
+plt.ylabel('Error (m)')
+plt.title('Error vs. time')
+plt.legend()
+
+# Show the plots
+plt.show()
